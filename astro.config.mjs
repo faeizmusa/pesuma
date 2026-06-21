4f9e27027dd94f8e10d49b353b4354398773abf3
Faeiz Musa <faeizmusa@gmail.com>
2026-06-21 15:57:28 +0800

    update robots & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,16 @@ import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://pesuma.org.my',
+  site: 'https://pesuma.com.my',
   output: 'server',
   adapter: cloudflare({
     imageService: 'compile',
   }),
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
